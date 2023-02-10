0b1767f441f1dd0a02c1f82be8164c4bd888ceb7
Tanmoy Saha <tanmoytssaha@gmail.com>
2023-02-11 01:02:14 +0530

    disapeared some secret key

--- DjangoAPI/settings.py
+++ DjangoAPI/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +23,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'Photos')
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3&hth++4w-tsyo!d=y6b-x)yu5$%lqiqm90)v=2o$ezbb2mrw@'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -89,8 +90,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'djongo',
         'CLIENT': {
-            "host":"mongodb+srv://tanmoytssaha:TanmoySaha@cluster0.mkr58ng.mongodb.net/?retryWrites=true&w=majority",
-            "name":"mytestdb",
+            "host": config("HOST_NAME"), 
+            "name": config("DATABASE_NAME"), 
             "authMechanism":"SCRAM-SHA-1", # FOR ATLAS CLOUD DB
         },
     }
